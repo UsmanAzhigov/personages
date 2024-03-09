e498b2ebb4a5fa390713f50e40f190b2bdfc0c91
Morfyyy <121609805+Morfyyy@users.noreply.github.com>
2024-03-09 21:45:29 +0300

    Сделал страницу входа, сделал страницу сброса, сделал функционал, исправил ошибки

---
+++ webpack.config.prod.js
@@ -0,0 +1,21 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'icons', to: 'icons' },
+        { from: 'js/vendor', to: 'js/vendor' },
+        { from: '404.html', to: '404.html' },
+        { from: 'site.webmanifest', to: 'site.webmanifest' },
+      ],
+    }),
+  ],
+});
