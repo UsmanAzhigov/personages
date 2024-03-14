5b387aec11eba4dd4947ff0d57c370825c314ed1
Morfyyy <121609805+Morfyyy@users.noreply.github.com>
2024-03-14 14:03:20 +0300

    fix

---
+++ scripts/reset-password-handler.js
@@ -0,0 +1,36 @@
+function handleResetPassword(
+  user_id,
+  timestamp,
+  signature,
+  password,
+  csrfToken,
+  base_url,
+  handleErrors,
+) {
+  fetch(`${base_url}accounts/reset-password/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRFToken': csrfToken,
+    },
+    body: JSON.stringify({
+      user_id,
+      timestamp,
+      signature,
+      password,
+    }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        window.location.href = './verify-reset-password.html';
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      handleErrors(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
