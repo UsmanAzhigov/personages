eaaa1815a7623f552c26d7e988ea601fb39dfc7d
Morfyyy <121609805+Morfyyy@users.noreply.github.com>
2024-03-18 14:45:17 +0300

    fix

--- scripts/token.js
+++ scripts/token.js
@@ -1,19 +1,12 @@
-function getCSRFTokenFromCookie(cookieName) {
-  var cookies = document.cookie.split(';');
-
-  for (var i = 0; i < cookies.length; i++) {
-    var cookie = cookies[i].trim();
-    var parts = cookie.split('=');
-    if (parts[0] === cookieName) {
-      return parts[1];
+function extractCsrfTokenFromCookie() {
+  const cookies = document.cookie.split(';');
+  for (let cookie of cookies) {
+    const [name, value] = cookie.trim().split('=');
+    if (name === 'csrftoken') {
+      return value;
     }
   }
   return null;
 }
-
-var csrfToken = getCSRFTokenFromCookie('csrftoken');
-if (csrfToken) {
-  console.log('CSRF токен:', csrfToken);
-} else {
-  console.log('CSRF токен не найден.');
-}
+const csrfToken = extractCsrfTokenFromCookie();
+console.log('csrftoken:', csrfToken);
